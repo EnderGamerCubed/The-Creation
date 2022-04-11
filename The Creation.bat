dc822180e5cdda94bb2dcb5260be4034ad82f0d7
FuzzyDice56 <103453267+FuzzyDice56@users.noreply.github.com>
2022-04-11 13:46:06 -0600

    add more code

--- The Creation.bat
+++ The Creation.bat
@@ -7,17 +7,21 @@ echo Welcome to the Creation, I am the Creator and you are the Existance
 pause
 
 echo Tell me, can you remember anything?
-PING -n 2 127.0.0.1>nul
 choice /c YN /t 10 /d N /m Yes or No?
 pause
 
 echo That is expected, you will have to rebuild your memories in the Creation
-PING -n 2 127.0.0.1>nul
 pause
 
 echo I am going to send you into the Creation now, remember that knowledge is key to survival...
-PING -n 2 127.0.0.1>nul
 pause
 CLS
 
 echo [NPC MAN 1] Hello, I'm NAMEHOLDER. I havent seen you around here before, what is your name? INSERTNAMEHERE
+pause
+
+echo [NPC MAN 1] Nice to meet you INSERTNAMEHERE, I will show you around..
+
+echo [NPC MAN 1] In the Creation you can talk to people, do jobs, and buy things, the goal for doing all this is to simply gain knowledge
+
+echo [NPC MAN 1] Knowledge is crutial to living in the Creation and without it you cannot live, you can lose knowledge by doing nothing for more then ten minutes
