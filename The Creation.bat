0ca7d3483be514030c804f9e2a3cc7d8bc75f006
EnderGamerCubed <derpbanana01@gmail.com>
2022-04-11 13:49:59 -0600

    E

--- The Creation.bat
+++ The Creation.bat
@@ -28,3 +28,4 @@ PING -n 2 127.0.0.1>nul
 echo [NPC MAN 1] In the Creation you can talk to people, do jobs, and buy things, the goal for doing all this is to simply gain knowledge
 PING -n 2 127.0.0.1>nul
 echo [NPC MAN 1] Knowledge is crutial to living in the Creation and without it you cannot live, you can lose knowledge by doing nothing for more then ten minutes
+pause
