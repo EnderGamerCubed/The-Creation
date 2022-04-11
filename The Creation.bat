b9f21b5ed519526eb2fd984a4c330464f61d2ee6
EnderGamerCubed <derpbanana01@gmail.com>
2022-04-11 13:27:07 -0600

    Update The Creation.bat
    Choice Command

--- The Creation.bat
+++ The Creation.bat
@@ -2,12 +2,12 @@
 Title Creation
 echo Hello Player, View The README File And Continue....
 pause
-
+CLS
 echo Welcome to the Creation, I am the Creator and you are the Existance
 pause
 
 echo Tell me, can you remember anything?
-choice Y or N
+choice /c YN /t 10 /d N /m Yes or No?
 pause
 
 echo That is expected, you will have to rebuild your memories in the Creation
@@ -15,6 +15,6 @@ pause
 
 echo I am going to send you into the Creation now, remember that knowledge is key to survival...
 pause
-
+CLS
 
 echo [NPC MAN 1] Hello, I'm NAMEHOLDER. I havent seen you around here before, what is your name? INSERTNAMEHERE
