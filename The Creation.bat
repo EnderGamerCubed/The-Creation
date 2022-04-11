3bc9b1ffb8830ea1becf4edaf0b309d10e58385b
EnderGamerCubed <derpbanana01@gmail.com>
2022-04-11 13:39:04 -0600

    PING

--- The Creation.bat
+++ The Creation.bat
@@ -7,13 +7,16 @@ echo Welcome to the Creation, I am the Creator and you are the Existance
 pause
 
 echo Tell me, can you remember anything?
+PING -n 2 127.0.0.1>nul
 choice /c YN /t 10 /d N /m Yes or No?
 pause
 
 echo That is expected, you will have to rebuild your memories in the Creation
+PING -n 2 127.0.0.1>nul
 pause
 
 echo I am going to send you into the Creation now, remember that knowledge is key to survival...
+PING -n 2 127.0.0.1>nul
 pause
 CLS
 
