34bb41949d176c738a1567ef7096581f4d71519a
FuzzyDice56 <103453267+FuzzyDice56@users.noreply.github.com>
2022-04-11 13:22:07 -0600

    Update The Creation.bat

--- The Creation.bat
+++ The Creation.bat
@@ -1,4 +1,17 @@
 @echo off
 Title Creation
-echo Hello Player, View the README file and continue....
+echo Hello Player, View The README File And Continue....
+pause
+
+echo Welcome to the Creation, I am the Creator and you are the Existance
+pause
+
+echo Tell me, can you remember anything?
+choice Y or N
+pause
+
+echo That is expected, you will have to rebuild your memories in the Creation
+pause
+
+echo I am going to send you into the Creation now, remember that knowledge is key to survival...
 pause
